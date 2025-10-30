a9fbae9ea2cf54b881a530943b7bc5bfcb518ff2
Kevin Liang <kevinlia@google.com>
2025-10-30 14:11:24 -0700

    typo 90G

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 dotnet app.dll &
 
 # Start FIO to generate load and log the output
-fio --name=standard-test --filename=/app/logs/testfile.dat --ioengine=libaio --size=G --verify=0 --randrepeat=0 --bs=4k --direct=1 --rw=randrw --rate=51200k,51200k --time_based --runtime=7d --numjobs=1 --iodepth=32 --output=/app/logs/fio-results.log
+fio --name=standard-test --filename=/app/logs/testfile.dat --ioengine=libaio --size=90G --verify=0 --randrepeat=0 --bs=4k --direct=1 --rw=randrw --rate=51200k,51200k --time_based --runtime=7d --numjobs=1 --iodepth=32 --output=/app/logs/fio-results.log
 
 # Wait for any process to exit
 wait -n
