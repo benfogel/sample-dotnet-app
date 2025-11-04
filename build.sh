30ba602aebcb6ef414c5198cd94e9162f31ed4a5
Kevin Liang <kevinlia@google.com>
2025-11-04 13:46:08 -0800

    Update: replace fio with write

--- build.sh
+++ build.sh
@@ -2,19 +2,20 @@
 
 # Check if a version argument is provided
 if [ -z "$1" ]; then
-  echo "Usage: ./build.sh <version>"
+  echo "Usage: ./build.sh <version> [dockerfile]"
   exit 1
 fi
 
 VERSION=$1
+DOCKERFILE=${2:-Dockerfile}
 IMAGE_NAME="gcr.io/cloud-alchemists-sandbox/kamek/sample-dotnet-app"
 
 TAG="${IMAGE_NAME}:${VERSION}"
 
-echo "Building Docker image ${TAG}..."
+echo "Building Docker image ${TAG} using ${DOCKERFILE}..."
 
 # Build the Docker image
-docker build -t "${TAG}" .
+docker build -f "${DOCKERFILE}" -t "${TAG}" .
 
 echo "Build complete."
 
--- start.sh
+++ start.sh
@@ -3,9 +3,18 @@
 # Start the dotnet application in the background
 dotnet app.dll &
 
-# Start FIO to generate load and log the output
-echo "--- Running FIO with $FIO_SIZE  ---"
-fio --name=standard-test --filename=/app/logs/testfile.dat --ioengine=libaio --size=${FIO_SIZE:-10G} --verify=0 --randrepeat=0 --bs=4k --direct=1 --rw=randrw --rate=51200k,51200k --time_based --runtime=7d --numjobs=1 --iodepth=32 --output=/app/logs/fio-results.log &
+# (
+# echo "--- Running FIO with $FIO_SIZE  ---"
+# fio --name=standard-test --filename=/app/logs/testfile.dat --ioengine=libaio --size=${FIO_SIZE:-10G} --verify=0 --randrepeat=0 --bs=4k --direct=1 --rw=randrw --rate=51200k,51200k --time_based --runtime=7d --numjobs=1 --iodepth=32 --output=/app/logs/fio-results.log 
+# ) &
+
+(
+  while true; do
+    echo "--- Writing to /app/logs/logfile continuously ---"
+    head -c 150G </dev/urandom >/app/logs/logfile
+  done
+) &
+
 
 # Give processes a moment to start
 sleep 2
@@ -16,25 +25,25 @@ sleep 2
   iostat -dx $(basename "$disk_device") 10
 ) &
 
-(
-  echo "--- Running pidstat for fio continuously ---"
-  fio_pids=$(pgrep -x fio | paste -sd ',')
-  if [[ -n "$fio_pids" ]]; then
-    pidstat -d -p "$fio_pids" 10
-  else
-    echo "fio process not found."
-  fi
-) &
+# (
+#   echo "--- Running pidstat for fio continuously ---"
+#   fio_pids=$(pgrep -x fio | paste -sd ',')
+#   if [[ -n "$fio_pids" ]]; then
+#     pidstat -d -p "$fio_pids" 10
+#   else
+#     echo "fio process not found."
+#   fi
+# ) &
 
-(
-  echo "--- Running pidstat for dotnet continuously ---"
-  dotnet_pids=$(pgrep -x dotnet | paste -sd ',')
-  if [[ -n "$dotnet_pids" ]]; then
-    pidstat -d -p "$dotnet_pids" 10
-  else
-    echo "dotnet process not found."
-  fi
-) &
+# (
+#   echo "--- Running pidstat for dotnet continuously ---"
+#   dotnet_pids=$(pgrep -x dotnet | paste -sd ',')
+#   if [[ -n "$dotnet_pids" ]]; then
+#     pidstat -d -p "$dotnet_pids" 10
+#   else
+#     echo "dotnet process not found."
+#   fi
+# ) &
 
 
 # Wait for any process to exit
